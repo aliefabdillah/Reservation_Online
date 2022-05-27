b8c3cf5fc666e6674cafb3db3c80f27e60945766
aliefabdillah <aliefmabdillah09@gmail.com>
2022-05-27 23:10:58 +0700

    update tittle Page

--- resources/views/v_testSeat.blade.php
+++ resources/views/v_testSeat.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <h1>TESTING SEAT</h1>
+    <h1>PILIH TEMPAT DUDUK</h1>
     @if(\Session::get('login') == TRUE)
         <a href="{{ route('customerLogout') }}"><button type="button" class="btn btn-danger float-end">Logout</button></a>
     @endif
