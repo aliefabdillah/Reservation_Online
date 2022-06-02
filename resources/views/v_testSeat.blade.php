324cf753306682ede3d5cd46aa220b192fd083b0
aliefabdillah <aliefmabdillah09@gmail.com>
2022-06-03 01:26:33 +0700

    update ui pilih seat

--- resources/views/v_testSeat.blade.php
+++ resources/views/v_testSeat.blade.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>Reservasi Online</title>
-=======
-    <title>Pilih Tempat Duduk</title>
->>>>>>> 2cdea66e96fa510c6478ff50e8130d357d6a8ca0
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
 		@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,500;0,700;1,400&family=Secular+One&display=swap');
